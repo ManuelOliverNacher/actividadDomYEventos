f2829b146410332b10291292912b00a1d1645325
ManuelOliverNacher <manueloliver6@gmail.com>
2022-06-20 19:58:10 -0300

    Entrega de desafio de Dom y Eventos

---
+++ Proyecto-Final Manuel-Oliver/js/main.js
@@ -0,0 +1,157 @@
+
+// Pestaña de Productos
+
+document.title= "Clase 9 / Eventos";
+titulo1.innerText= "Conoce Nuestros Productos con Eventos"
+
+const 
+btnPlease=document.querySelector(' #boton1 '),
+btnHappy=document.querySelector('#boton2'),
+btnAthletic=document.querySelector('#boton3'),
+btnBerkley=document.querySelector('#boton4'),
+btnCute=document.querySelector('#boton5'),
+btnBoston=document.querySelector('#boton6'),
+btnHawaii=document.querySelector('#boton7'),
+btn1977Black=document.querySelector('#boton8'),
+btn1977Green=document.querySelector('#boton9'),
+btnFutureBlue=document.querySelector('#boton10'),
+btnFutureBlack=document.querySelector('#boton11'),
+btnOriginal=document.querySelector('#boton12');
+
+btnPlease.addEventListener("click", () =>{
+alert("Agregaste el Buzo Please al carrito");
+}
+)
+
+btnHappy.addEventListener("click", () =>{
+    alert("Agregaste el Buzo Happy al carrito");
+}
+)
+
+btnAthletic.addEventListener("click", () =>{
+        alert("Agregaste el Buzo Athletic al carrito");
+ }
+ )
+
+ btnBerkley.addEventListener("click", () =>{
+    alert("Agregaste el Buzo Berkley al carrito");
+}
+)
+
+btnCute.addEventListener("click", () =>{
+    alert("Agregaste el Buzo Cute al carrito");
+}
+)
+
+btnBoston.addEventListener("click", () =>{
+    alert("Agregaste el Buzo Boston al carrito");
+}
+)
+
+btnHawaii.addEventListener("click", () =>{
+    alert("Agregaste el Buzo Hawaii al carrito");
+}
+)
+
+btn1977Black.addEventListener("click", () =>{
+    alert("Agregaste el Buzo Black 1977 al carrito");
+}
+)
+
+btn1977Green.addEventListener("click", () =>{
+    alert("Agregaste el Buzo Green 1977 al carrito");
+}
+)
+
+btnFutureBlue.addEventListener("click", () =>{
+    alert("Agregaste el Buzo Future Blue al carrito");
+}
+)
+
+btnFutureBlack.addEventListener("click", () =>{
+    alert("Agregaste el Buzo Future Black al carrito");
+}
+)
+
+btnOriginal.addEventListener("click", () =>{
+    alert("Agregaste el Buzo Original al carrito");
+}
+)
+
+// Termino de trabajar en la pestaña de productos
+
+//Pestaña de Contacto/Formulario
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* alert("Bienvenidos a la Tienda")
+
+const productos=[
+    {id:1, nombre:"BuzoOversize", precio:6500, categoria:"Buzos"},
+    {id:2, nombre:"BuzoAjustado", precio:6300, categoria:"Buzos"},
+    {id:3, nombre:"Remera", precio:3000, categoria:"Remera"},
+    {id:4, nombre:"Camperas", precio:7000, categoria:"Campera"},
+]
+console.log("-------------------------");
+
+console.log(productos)
+
+let comprarProd = prompt("Seleccione el precio del articulo que desea comprar\nBuzo Oversize: $6500\nBuzo Ajustado: $6300\nRemera: $3000\nCampera: $7000 \nAl valor del producto se le aumenta un monto de envio");
+comprarProd=parseFloat(comprarProd);
+
+switch (comprarProd){
+    case 6500:
+    alert("El buzo Oversize cuesta " + comprarProd);
+    break;
+
+    case 6300:
+    alert("El buzo Ajustado cuesta " + comprarProd);
+    break;
+
+    case 3000:
+    alert("La remera cuesta " + comprarProd);
+    break;
+
+    case 7000:
+    alert("La campera cuesta " + comprarProd);
+    break;
+
+    default:
+    alert("El precio ingresado es incorrecto ");
+    break;
+}
+let precioPagar= comprarProd;
+total=0;
+
+function envio (comprarProd) {
+
+total=(comprarProd + 500);
+    
+    return total;
+    
+    
+}
+alert("El precio final a pagar es " + envio(comprarProd) + " (incluye envio)\nGracias por su compra! ");*/
+    
+    
+
+
+
+
+
